9b9299617077195ab6e8146d5a70410bd6857a50
khouloud-kartal <khouloud.kartal@laplateforme.io>
2023-01-24 13:40:17 +0100

    jobs jour08

---
+++ jour08/job02/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Job 02</title>
+</head>
+<body class="w-screen">
+    <header class="w-screen bg-red-200 py-8">
+
+    <nav class="flex justify-around ">
+        <a href="" class="">Acceuil</a>
+        <a href="">inscription</a>
+        <a href="">Connexion</a>
+        <a href="">Recherche</a>
+    </nav>
+
+    </header>
+
+    <main>
+
+    <h2>Formulaire</h2>
+
+    <form action="">
+
+        <div class="civility">
+
+            <p>Select Your Civility</p>
+        
+            <input type="radio" name="civility" id="male">
+            <label for="male">Male</label><br>
+
+            <input type="radio" name="civility" id="female">
+            <label for="female">Female</label><br>
+
+            <input type="radio" name="civility" id="other">
+            <label for="other">Other</label><br>
+
+        </div>
+
+        <div class="formulaire">
+
+            <p>Fill All Your Information</p>
+
+            <input type="text" name="prenom" id="prenom">
+            <label for="prenom">First Name</label><br>
+
+            <input type="text" name="nom" id="nom">
+            <label for="nom">Last Name</label><br>
+
+            <input type="text" name="adresse" id="adress">
+            <label for="adresse">Adress</label><br>
+
+            <input type="email" name="email" id="email">
+            <label for="email">Adress Email</label><br>
+
+            <input type="password" name="password" id="password">
+            <label for="password">Password</label><br>
+
+            <input type="password" name="confirm_password" id="confirm_password">
+            <label for="confirm_password">Confirm Password</label><br>
+
+            <p>Choose your passions</p>
+            
+            <input type="checkbox" name="informatique" id="informatique">
+            <label for="informatique">Computer Science</label><br>
+
+            <input type="checkbox" name="voyage" id="voyage">
+            <label for="voyage">Traveling</label><br>
+
+            <input type="checkbox" name="sport" id="sport">
+            <label for="sport">Sport</label><br>
+
+            <input type="checkbox" name="lecture" id="lecture">
+            <label for="lecture">Reading</label><br>
+
+            <input type="button" value="valider">
+
+        </div>
+    </form>
+
+    </main>
+
+    <footer>
+
+    <nav>
+        <a href="">Acceuil</a>
+        <a href="">inscription</a>
+        <a href="">Connexion</a>
+        <a href="">Recherche</a>
+    </nav>
+
+    </footer>
+</body>
+</html>
---
+++ jour08/job04/index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Job 04</title>
+</head>
+<body class="w-full py-0 my-0">
+    <header class="w-screen bg-red-200 py-8">
+
+    <nav class="flex justify-around ">
+        <a href="" class="">Acceuil</a>
+        <a href="">inscription</a>
+        <a href="">Connexion</a>
+        <a href="">Recherche</a>
+    </nav>
+
+    </header>
+
+    <main class="w-screen flex justify-center py-6">
+        <div class="block p-6 rounded-lg shadow-lg bg-white max-w-md">
+            <form>
+            <div class="flex justify-center">
+                <p class="p-6">Civility:</p>
+                <div class="form-check form-check-inline p-6">
+                    <input class="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                    <label class="form-check-label inline-block text-gray-800" for="inlineRadio10">male</label>
+                </div>
+
+                <div class="form-check form-check-inline p-6">
+                    <input class="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                    <label class="form-check-label inline-block text-gray-800" for="inlineRadio20">female</label>
+                </div>
+                </div>
+                <div class="grid grid-cols-2 gap-4">
+                <div class="form-group mb-6">
+                    <input type="text" class="form-control
+                    block
+                    w-full
+                    px-3
+                    py-1.5
+                    text-base
+                    font-normal
+                    text-gray-700
+                    bg-white bg-clip-padding
+                    border border-solid border-gray-300
+                    rounded
+                    transition
+                    ease-in-out
+                    m-0
+                    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput123"
+                    aria-describedby="emailHelp123" placeholder="First name">
+                </div>
+                <div class="form-group mb-6">
+                    <input type="text" class="form-control
+                    block
+                    w-full
+                    px-3
+                    py-1.5
+                    text-base
+                    font-normal
+                    text-gray-700
+                    bg-white bg-clip-padding
+                    border border-solid border-gray-300
+                    rounded
+                    transition
+                    ease-in-out
+                    m-0
+                    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput124"
+                    aria-describedby="emailHelp124" placeholder="Last name">
+                </div>
+                </div>
+                <div class="form-group mb-6">
+                <input type="text" class="form-control block
+                    w-full
+                    px-3
+                    py-1.5
+                    text-base
+                    font-normal
+                    text-gray-700
+                    bg-white bg-clip-padding
+                    border border-solid border-gray-300
+                    rounded
+                    transition
+                    ease-in-out
+                    m-0
+                    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput125"
+                    placeholder="Adress">
+                </div>
+                <div class="form-group mb-6">
+                <input type="email" class="form-control block
+                    w-full
+                    px-3
+                    py-1.5
+                    text-base
+                    font-normal
+                    text-gray-700
+                    bg-white bg-clip-padding
+                    border border-solid border-gray-300
+                    rounded
+                    transition
+                    ease-in-out
+                    m-0
+                    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput125"
+                    placeholder="Email address">
+                </div>
+                <div class="form-group mb-6">
+                <input type="password" class="form-control block
+                    w-full
+                    px-3
+                    py-1.5
+                    text-base
+                    font-normal
+                    text-gray-700
+                    bg-white bg-clip-padding
+                    border border-solid border-gray-300
+                    rounded
+                    transition
+                    ease-in-out
+                    m-0
+                    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput126"
+                    placeholder="Password">
+                </div>
+                <div class="form-group mb-6">
+                <input type="password" class="form-control block
+                    w-full
+                    px-3
+                    py-1.5
+                    text-base
+                    font-normal
+                    text-gray-700
+                    bg-white bg-clip-padding
+                    border border-solid border-gray-300
+                    rounded
+                    transition
+                    ease-in-out
+                    m-0
+                    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput126"
+                    placeholder="Confirm Password">
+                </div>
+                <div>
+                    <p>Chouse your passions:</p><br>
+                </div>
+                <div class="flex justify-start">
+                <div>
+                    <div class="form-check">
+                    <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
+                    <label class="form-check-label inline-block text-gray-800" for="flexCheckDefault">
+                        Computer Science
+                    </label>
+                    </div>
+                    <div class="form-check">
+                    <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckChecked" checked>
+                    <label class="form-check-label inline-block text-gray-800" for="flexCheckChecked">
+                        Traveling
+                    </label>
+                    </div>
+                    <div class="form-check">
+                    <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
+                    <label class="form-check-label inline-block text-gray-800" for="flexCheckDefault">
+                        Sport
+                    </label>
+                    </div>
+                    <div class="form-check">
+                    <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault">
+                    <label class="form-check-label inline-block text-gray-800" for="flexCheckDefault">
+                        Reading
+                    </label>
+                    </div>
+                </div>
+                </div>
+                <button type="submit" class="
+                w-full
+                px-6
+                py-2.5
+                my-4
+                bg-blue-600
+                text-white
+                font-medium
+                text-xs
+                leading-tight
+                uppercase
+                rounded
+                shadow-md
+                hover:bg-blue-700 hover:shadow-lg
+                focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+                active:bg-blue-800 active:shadow-lg
+                transition
+                duration-150
+                ease-in-out">Sign up</button>
+            </form>
+        </div>
+
+    </main>
+
+    <footer class="w-screen bg-neutral-400 py-8 ">
+
+    <ul class="flex justify-around">
+        <li><a href="">Acceuil</a></li>
+        <li><a href="">inscription</a></li>
+        <li><a href="">Connexion</a></li>
+        <li><a href="">Recherche</a></li>
+    </ul>
+
+    </footer>
+</body>
+</html>
